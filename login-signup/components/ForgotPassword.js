a37c9b9c43262ecae1291985e1143ef2f3f8b54a
Ravin yasas jayasekara <it21360046@my.sliit.lk>
2025-03-16 23:11:12 +0530

    Proper Error Handling and smooth navigation

--- login-signup/components/ForgotPassword.js
+++ login-signup/components/ForgotPassword.js
@@ -1,105 +1,77 @@
 import React, { Component } from 'react';
-import { Alert, Button, TextInput, View, StyleSheet, TouchableOpacity, Text, Image } from 'react-native';
+import { Alert, TextInput, View, StyleSheet, TouchableOpacity, Text, Image } from 'react-native';
 import 'react-native-gesture-handler';
 import AwesomeAlert from 'react-native-awesome-alerts';
 import { getAuth, sendPasswordResetEmail } from 'firebase/auth';
 
-export default class ForgotPassword extends React.Component {
-
-  static navigationOptions = ({ navigation }) => {
-    return {
-      headerTitle: 'Help Banana',
-      headerStyle: { backgroundColor: '#2BAB32' },
-      headerTintColor: '#ffffff',
-      headerLeft: () => {
-        return null;
-      }
-    }
-  };
+export default class ForgotPassword extends Component {
 
   constructor(props) {
     super(props);
-
     this.state = {
       email: '',
-      password: '',
       message: '',
       showAlert: false,
       title: ''
     };
   }
 
-  onSend = e => {
-
+  onSend = () => {
     const auth = getAuth();
-
     sendPasswordResetEmail(auth, this.state.email)
       .then(() => {
         this.setState({ title: "Success!", message: "Please check your email...!" })
-        this.showAlert()
+        this.showAlert();
       })
       .catch(error => {
         this.setState({ title: "Error!", message: error.message })
-        this.showAlert()
-      }
-      );
-
+        this.showAlert();
+      });
   }
 
   showAlert = () => {
-    this.setState({
-      showAlert: true
-    });
+    this.setState({ showAlert: true });
   };
 
   hideAlert = () => {
-    this.setState({
-      showAlert: false,
-      message: '',
-      title: ''
-    })
-    this.props.navigation.navigate('Login')
+    this.setState({ showAlert: false, message: '', title: '' });
+    this.props.navigation.navigate('Login');
   };
 
   render() {
-    const { showAlert } = this.state;
     return (
       <View style={styles.container}>
-
         <View style={styles.card}>
-        <Text style={styles.title}>Forgot Password</Text>
-          <View>
-            <TextInput
-              style={styles.input}
-              placeholder="Email"
-              value={this.state.email}
-              onChangeText={(email) => this.setState({ email })}
-            />
-            <TouchableOpacity style={styles.loginButton} onPress={this.onSend}>
-              <Text style={styles.loginButtonText}>Send</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.registerButton} onPress={() => this.props.navigation.replace("Login")}>
-              <Text style={styles.regButtonText}>Lgoin</Text>
-            </TouchableOpacity>
-          </View>
+          <Text style={styles.title}>Forgot Password</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Enter your email"
+            value={this.state.email}
+            onChangeText={(email) => this.setState({ email })}
+          />
+          <TouchableOpacity style={styles.loginButton} onPress={this.onSend}>
+            <Text style={styles.loginButtonText}>Send</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.registerButton} onPress={() => this.props.navigation.replace("Login") }>
+            <Text style={styles.regButtonText}>Login</Text>
+          </TouchableOpacity>
         </View>
-                <Text style={styles.orText}>or</Text>
-                <View style={styles.socialContainer}>
-                  <TouchableOpacity>
-                    <Image source={require('./../assets/facebook.png')} style={styles.socialIcon} />
-                  </TouchableOpacity>
-                  <TouchableOpacity>
-                    <Image source={require('./../assets/twitter.png')} style={styles.socialIcon} />
-                  </TouchableOpacity>
-                  <TouchableOpacity>
-                    <Image source={require('./../assets/google.png')} style={styles.socialIcon} />
-                  </TouchableOpacity>
-                </View>
-        
-                <Image source={require('./../assets/banana.png')} style={styles.bottomImage} />
+        <Text style={styles.orText}>or</Text>
+        <View style={styles.socialContainer}>
+          <TouchableOpacity>
+            <Image source={require('../assets/facebook.png')} style={styles.socialIcon} />
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Image source={require('../assets/twitter.png')} style={styles.socialIcon} />
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Image source={require('../assets/google.png')} style={styles.socialIcon} />
+          </TouchableOpacity>
+        </View>
+        <Image source={require('../assets/banana.png')} style={styles.bottomImage} />
 
         <AwesomeAlert
-          show={showAlert}
+          show={this.state.showAlert}
           title={this.state.title}
           message={this.state.message}
           closeOnTouchOutside={true}
@@ -107,11 +79,8 @@ export default class ForgotPassword extends React.Component {
           showCancelButton={true}
           cancelText="Close"
           cancelButtonColor="#AEDEF4"
-          onCancelPressed={() => {
-            this.hideAlert()
-          }}
+          onCancelPressed={this.hideAlert}
         />
-
       </View>
     );
   }
@@ -122,7 +91,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#f2f2f2',
+    backgroundColor: '#f8f9fa',
     padding: 20,
   },
   card: {
@@ -130,11 +99,11 @@ const styles = StyleSheet.create({
     padding: 20,
     borderRadius: 20,
     backgroundColor: '#fff',
-    elevation: 10,
+    elevation: 8,
     shadowColor: '#000',
-    shadowOffset: { width: 0, height: 4 },
+    shadowOffset: { width: 0, height: 3 },
     shadowOpacity: 0.3,
-    shadowRadius: 10,
+    shadowRadius: 8,
   },
   title: {
     fontSize: 24,
@@ -142,48 +111,24 @@ const styles = StyleSheet.create({
     marginBottom: 20,
     textAlign: 'center',
   },
-  switchContainer: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    marginBottom: 20,
-    backgroundColor: '#e1e1e1',
-    borderRadius: 30,
-    overflow: 'hidden',
-  },
-  switchButton: {
-    paddingVertical: 10,
-    paddingHorizontal: 30,
-  },
-  activeButton: {
-    backgroundColor: '#bbb',
-  },
-  switchButtonText: {
-    fontSize: 16,
-    color: '#000',
-  },
   input: {
     width: '100%',
     height: 45,
-    borderColor: '#ddd',
+    borderColor: '#ced4da',
     borderWidth: 1,
     borderRadius: 10,
     paddingHorizontal: 10,
     marginVertical: 10,
   },
-  forgotPassword: {
-    textAlign: 'right',
-    color: '#666',
-    marginBottom: 15,
-  },
   loginButton: {
-    backgroundColor: '#665eff',
+    backgroundColor: '#007bff',
     padding: 15,
     borderRadius: 10,
     alignItems: 'center',
     marginVertical: 10,
   },
   registerButton: {
-    backgroundColor: '#7bffa2',
+    backgroundColor: '#6c757d',
     padding: 15,
     borderRadius: 10,
     alignItems: 'center',
@@ -194,12 +139,12 @@ const styles = StyleSheet.create({
     fontSize: 18,
   },
   regButtonText: {
-    color: '#000',
+    color: '#fff',
     fontSize: 18,
   },
   orText: {
     marginVertical: 15,
-    color: '#999',
+    color: '#6c757d',
     fontSize: 16,
   },
   socialContainer: {
